747ba6d2de7d45ad477c5086580c866ec8cbf6ef
Valeria Tonetto <tonetto@arpage.ch>
2020-01-13 12:11:08 +0100

    Working download of image file at "rest-api/endpoint-1/get".

--- src/main/java/ch/arpage/valeriatonetto/WalletPassService.java
+++ src/main/java/ch/arpage/valeriatonetto/WalletPassService.java
@@ -6,8 +6,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.*;
 import javax.imageio.*;
 import java.io.*;
 
@@ -15,34 +14,87 @@ import java.io.*;
 @Path("/")
 public class WalletPassService {
 
-//    @GET
-//    @Path("{clientName}")
-//    @Produces(MediaType.TEXT_PLAIN)
-//    public Response greetClient(@PathParam("clientName") String name) {
-//        String output = "Hi " + name;
-//        return Response.status(200).entity(output).build();
-//    }
+    @Context ServletContext servletContext;
+
+    @GET
+    @Path("example/{clientName}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response greetClient(@PathParam("clientName") String name) {
+        String output = "Hi " + name;
+        return Response.status(200).entity(output).build();
+    }
 
     @GET
     @Path("get")
-    @Produces("image/png")
-    public Response displayImage(HttpServletRequest req, HttpServletResponse resp, ServletContext servletContext) {
-        String filename = ""
-        File file = new File(filename);
-        resp.setContentLength((int)file.length());
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public Response displayImage() {
 
-        FileInputStream in = new FileInputStream(file);
-        OutputStream out = resp.getOutputStream();
-
-        // Copy the contents of the file to the output stream
-        byte[] buf = new byte[1024];
-        int count = 0;
-        while ((count = in.read(buf)) >= 0) {
-            out.write(buf, 0, count);
-        }
-        out.close();
-        in.close();
-
-        return Response.status(200).entity(output).build();
+//        ServletContext cntx= req.getServletContext();
+//        // Get the absolute path of the image
+//        String filename = cntx.getRealPath("src/main/webapp/images/image.png");
+//        // retrieve mimeType dynamically
+//        String mime = cntx.getMimeType(filename);
+//        if (mime == null) {
+//            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+//            return Response.status(500).entity(String.valueOf(resp)).build();
+//        }
+//
+//        resp.setContentType(mime);
+//
+//        File file = new File(filename);
+//        resp.setContentLength((int)file.length());
+//
+//        FileInputStream in = null;
+//        try {
+//            in = new FileInputStream(file);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//
+//        OutputStream out = null;
+//        try {
+//            out = resp.getOutputStream();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        // Copy the contents of the file to the output stream
+//        byte[] buf = new byte[1024];
+//        int count = 0;
+//        try {
+//            if (in != null) {
+//                while ((count = in.read(buf)) >= 0) {
+//                    if (out != null) {
+//                        out.write(buf, 0, count);
+//                    }
+//                }
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            if (out != null) {
+//                out.close();
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            if (in != null) {
+//                in.close();
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return Response.status(200).entity(String.valueOf(resp)).build();
+//    }
+        String filepath = servletContext.getRealPath("images/image.png");
+        File file = new File(filepath);
+        return Response.ok(file, MediaType.APPLICATION_OCTET_STREAM)
+                .header("Content-Disposition", "attachment; filename=\"" + file.getName() + "\"") //optional
+                .build();
     }
 }
